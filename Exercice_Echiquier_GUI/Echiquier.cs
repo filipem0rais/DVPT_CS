20f7e5dc3d509cefdfca55020a6b7509cccc6f9d
fildiasmora <filipe.diasmorais@gmail.com>
2022-11-29 14:27:50 +0100

    Exercices echiquiers

---
+++ Exercice_Echiquier_GUI/Echiquier.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_Echiquier_GUI
+{
+    public enum ChessPiece
+    {
+        vide,
+        BDame,
+        BFou,
+        BCavalier,
+        BTour,
+        BPion,
+        BRoi,
+        NDame,
+        NFou,
+        NCavalier,
+        NTour,
+        NPion,
+        NRoi
+    }
+
+    internal class Echiquier
+    {
+        public Echiquier()
+        {
+        }
+
+        public Array GetEchiquierArray()
+        {
+            Array chessBoard = Array.CreateInstance(typeof(ChessPiece), 8, 8);
+            fillChessboard(chessBoard);
+
+            return chessBoard;
+        }
+
+        private void fillChessboard(Array chessBoard)
+        {
+            // fill the chessboard with pieces
+            chessBoard.SetValue(ChessPiece.BRoi, 0, 4);  // black king
+            chessBoard.SetValue(ChessPiece.NRoi, 7, 4);  // white king
+            chessBoard.SetValue(ChessPiece.BDame, 0, 3); // black queen
+            chessBoard.SetValue(ChessPiece.NDame, 7, 3); // white queen
+            chessBoard.SetValue(ChessPiece.BFou, 0, 2);  // black bishop
+            chessBoard.SetValue(ChessPiece.BFou, 0, 5);  // black bishop
+            chessBoard.SetValue(ChessPiece.NFou, 7, 2);  // white bishop
+            chessBoard.SetValue(ChessPiece.NFou, 7, 5);  // white bishop
+            chessBoard.SetValue(ChessPiece.BCavalier, 0, 1); // black knight
+            chessBoard.SetValue(ChessPiece.BCavalier, 0, 6); // black knight
+            chessBoard.SetValue(ChessPiece.NCavalier, 7, 1); // white knight
+            chessBoard.SetValue(ChessPiece.NCavalier, 7, 6); // white knight
+            chessBoard.SetValue(ChessPiece.BTour, 0, 0); // black rook
+            chessBoard.SetValue(ChessPiece.BTour, 0, 7); // black rook
+            chessBoard.SetValue(ChessPiece.NTour, 7, 0); // white rook
+            chessBoard.SetValue(ChessPiece.NTour, 7, 7); // white rook
+
+            // fill the chessboard with pawns
+            for (int i = 0; i < 8; i++)
+            {
+                chessBoard.SetValue(ChessPiece.BPion, 1, i); // black pawn
+                chessBoard.SetValue(ChessPiece.NPion, 6, i); // white pawn
+            }
+
+            // fill the chessboard with empty squares
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (chessBoard.GetValue(i, j) == null)
+                    {
+                        chessBoard.SetValue(ChessPiece.vide, i, j);
+                    }
+                }
+            }
+        }
+    }
+
+
+
+}
---
+++ Exercice_Echiquier_GUI/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercice_Echiquier_GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Echiquier chessBoard = new Echiquier();
+
+            Array chessBoardArray = chessBoard.GetEchiquierArray();
+
+            displayChessboard(chessBoardArray);
+
+
+        }
+
+        private void displayChessboard(Array chessBoardArray)
+        {
+            // display the chessboard
+            for (int i = 0; i < chessBoardArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < chessBoardArray.GetLength(1); j++)
+                {
+                    Button myButton = new Button();
+                    myButton.Content = chessBoardArray.GetValue(i, j);
+                    myButton.Name = "Button" + i + j;
+                    if ((i + j) % 2 == 0)
+                    {
+                        myButton.Background = Brushes.White;
+                        myButton.Foreground = Brushes.Black;
+                        
+                    }
+                    else
+                    {
+                        myButton.Background = Brushes.Black;
+                        myButton.Foreground = Brushes.White;
+                    }
+
+                   // BitmapImage btm = new BitmapImage(new Uri("/Exercice_Echiquier_GUI;component/Images/" + chessBoardArray.GetValue(i, j) + ".png", UriKind.Relative));
+                   // Image img = new Image();
+                   // img.Source = btm;
+                   // myButton.Content = img;
+
+
+                    Grid.SetRow(myButton, i);
+                    Grid.SetColumn(myButton, j);
+                    gridMain.Children.Add(myButton);                    
+                }
+            }
+        }
+    }
+}
